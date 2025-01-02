94d4238faf7c90b0413c0f55cdda125d92f1a5be
Alexander Goussas <goussasalexander@gmail.com>
2025-01-02 10:29:33 -0500

    add rate limitting

--- src/main/java/io/github/aloussase/alexandria/application/controller/BookController.java
+++ src/main/java/io/github/aloussase/alexandria/application/controller/BookController.java
@@ -3,6 +3,9 @@ package io.github.aloussase.alexandria.application.controller;
 import io.github.aloussase.alexandria.domain.models.Book;
 import io.github.aloussase.alexandria.domain.services.BookService;
 
+import io.github.resilience4j.ratelimiter.RequestNotPermitted;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Slf4j
 @RequestMapping("/api/books")
 @RestController
 public class BookController {
@@ -21,6 +25,7 @@ public class BookController {
         this.bookService = bookService;
     }
 
+    @RateLimiter(name = "books-backend", fallbackMethod = "tooManyRequests")
     @GetMapping
     public ResponseEntity<List<Book>> searchBooks(
             @RequestParam("title") String title
@@ -28,4 +33,8 @@ public class BookController {
         return ResponseEntity.ok(bookService.search(title));
     }
 
+    private ResponseEntity<List<Book>> tooManyRequests(String title, RequestNotPermitted ex) {
+        log.warn("too many requests for {}", title);
+        return ResponseEntity.status(429).build();
+    }
 }
